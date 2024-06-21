5d380f29617d1add9f192becfd4f878212bc8622
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2024-06-21 17:31:16 -0300

    up

--- Hello Cloud/lgpd-back/repositories/UserRepository.js
+++ Hello Cloud/lgpd-back/repositories/UserRepository.js
@@ -1,12 +1,12 @@
 import User from "../models/User.js";
 
-//1- salvar usuario
+// Salvar usuario
 const saveUser = async (UserModel) => {
   const save = await User.create(UserModel);
   return save;
 }
 
-//2- buscar todos usuarios
+// Buscar todos usuarios
 const getAllUsers = async () => {
   return await User.findAll({
     order: [
@@ -15,24 +15,24 @@ const getAllUsers = async () => {
   });
 };
 
-//3- buscar por id
+// Buscar por id
 const getUserById = async (id) => {
   return await User.findByPk(id); //primary key
 };
 
-//4- deletar por id
+// Deletar por id
 const deleteUserById = async (id) => {
   return await User.destroy({ where: { id: id } }); 
   //primeiro é o atributo do banco segundo é o parametro da função
 };
 
-//5- atualizar por id
+// Atualizar por id
 const updateUserById = async (id, UserModel) => {
   try {
     const result = await User.update(UserModel, { where: { id: id } });
     //primeiro é o atributo do banco segundo é o parametro da função
     if (result[0] === 1) {
-      return { message: "User updated with success" };
+      return { message: "User updated successully" };
     } else {
       return { message: `Can't find User ${id} to update`, status: 404 };
     }
--- Hello Cloud/lgpd-back/routes/UserController.js
+++ Hello Cloud/lgpd-back/routes/UserController.js
@@ -1,6 +1,53 @@
 import express from "express";
 let router = express.Router();
 
-import teacherService from "../services/TeacherService.js";
+import userService from "../services/UserService.js";
+
+// salvar usuario
+router.post("/addUser", async function (req, res) {
+  const userModel = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    gender: req.body.gender,
+  }
+  const user = await userService.saveUser(userModel);
+  return res.status(200).json(user);
+});
+
+// buscar todos usuarios
+router.get("/getAllUsers", async function (req, res) {
+  const allUsers = await userService.getAllUsers();
+  return res.status(200).json(allUsers);
+});
+
+// buscar por id
+router.get("/user/:id", async function (req, res) {
+  const user = await userService.getUserById(req.params.id);
+  return res.status(200).json(user);
+});
+
+// deletar por id
+router.delete("/deleteUser/:id", async function (req, res) {
+  const user = await userService.deleteUserById(req.params.id);
+  return res.status(200).json(user);
+});
+
+//  atualizar por id
+router.put("/updateUser/:id", async function (req, res) {
+  const userModel = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    gender: req.body.gender,
+  }
+  const user = await userService.updateUserById(req.params.id, userModel);
+  return res.status(200).json(user);
+});
+
+router.get('/userImage/:id', async function (req, res){
+  const user = await userService.getUserById(req.params.id);
+  res.sendFile(process.cwd() + "\\" + user.profile_picture);
+});
 
 export default router;
