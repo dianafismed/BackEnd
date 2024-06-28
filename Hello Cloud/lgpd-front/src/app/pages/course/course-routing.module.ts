54d41078b112dcf2e29a91f443d04cf7d066609f
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2024-06-28 11:09:53 -0300

    up

--- Hello Cloud/lgpd-front/src/app/pages/course/course-routing.module.ts
+++ Hello Cloud/lgpd-front/src/app/pages/course/course-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//Add esses imports
 import {CourseFormComponent} from "./course-form/course-form.component";
 import {CourseListComponent} from "./course-list/course-list.component";
 
-
-//Alterar routes para adicionar as rotas /courses e /addCourse
 const routes: Routes = [
-  {
-    path: 'courses',
-    component: CourseListComponent
-  },
-  {
-    path: 'addCourse',
-    component: CourseFormComponent
-  }];
+  {path: 'courses',component: CourseListComponent},
+  {path: 'addCourse',component: CourseFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
